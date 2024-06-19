b97578a0d692124b897fe9e30966778d29b5f5ab
tduprez <tduprez@theocomputer.home>
2024-06-19 23:09:50 +0200

    feat(player-in-bracket-component): Font size is now dynamic, whe it\'s at 10px, the text is troncate by '...'

--- src/requirements/frontend/static/js/components/Game/states/tournamentHome/bracket/PlayerInBracketComponent.js
+++ src/requirements/frontend/static/js/components/Game/states/tournamentHome/bracket/PlayerInBracketComponent.js
@@ -1,6 +1,6 @@
 class PlayerInBracketComponent extends HTMLElement {
     static get observedAttributes() {
-        return ['name', 'score', 'class'];
+        return ['name', 'score', 'class', 'left', 'right'];
     }
 
     constructor() {
@@ -16,6 +16,21 @@ class PlayerInBracketComponent extends HTMLElement {
         `;
 
         this.className =  this.getAttribute('class');
+    }
+
+    connectedCallback() {
+        const nameContainer = this.querySelector('.player-name');
+        const name = this.querySelector('.player-name > p');
+
+        let i = 0;
+        console.log(parseFloat(getComputedStyle(name).fontSize));
+        while (name.offsetWidth > nameContainer.offsetWidth - 4 && parseFloat(getComputedStyle(name).fontSize) >= 10) {
+            name.style.fontSize = `${parseFloat(getComputedStyle(name).fontSize) - 1}px`;
+            i++;
+        }
+        if (name.offsetWidth > nameContainer.offsetWidth - 4) {
+            name.className = 'overflow-name';
+        }
     }
 
 }
--- src/requirements/frontend/static/js/components/Game/states/tournamentHome/bracket/bracket.js
+++ src/requirements/frontend/static/js/components/Game/states/tournamentHome/bracket/bracket.js
@@ -10,7 +10,7 @@ class bracket {
     render() {
         return `
             <div class="left-matches">
-                <player-in-bracket name="Sowoo" score="11" id="left-height-player1" class="player-in-bracket-game-win"></player-in-bracket>
+                <player-in-bracket name="Sowoo" score="11" id="left-height-player1" class="player-in-bracket-himself"></player-in-bracket>
                 <player-in-bracket name="Hleung" score="1" id="left-height-player2" class="player-in-bracket-basic"></player-in-bracket>
                 <player-in-bracket name="Acarlott" score="7" id="left-height-player3" class="player-in-bracket-basic"></player-in-bracket>
                 <player-in-bracket name="Pgouasmi" score="11" id="left-height-player4" class="player-in-bracket-game-win"></player-in-bracket>
@@ -19,35 +19,35 @@ class bracket {
                 <player-in-bracket name="Natterie" score="11" id="left-height-player7" class="player-in-bracket-game-win"></player-in-bracket>
                 <player-in-bracket name="Tduprez" score="9" id="left-height-player8" class="player-in-bracket-basic"></player-in-bracket>
                 
-                <player-in-bracket name="Sowoo" score="10" id="left-four-player1" class="player-in-bracket-basic"></player-in-bracket>
+                <player-in-bracket name="Sowoo" score="10" id="left-four-player1" class="player-in-bracket-himself"></player-in-bracket>
                 <player-in-bracket name="Sowoo" score="11" id="left-four-player2" class="player-in-bracket-game-win"></player-in-bracket>
                 <player-in-bracket name="Sowoo" score="9" id="left-four-player3" class="player-in-bracket-basic"></player-in-bracket>
                 <player-in-bracket name="Sowoo" score="11" id="left-four-player4" class="player-in-bracket-game-win"></player-in-bracket>
                 
-                <player-in-bracket name="Sowoo" score="11" id="left-two-player1" class="player-in-bracket-game-win"></player-in-bracket>
+                <player-in-bracket name="Sowoo" score="11" id="left-two-player1" class="player-in-bracket-himself"></player-in-bracket>
                 <player-in-bracket name="Sowoo" score="10" id="left-two-player2" class="player-in-bracket-basic"></player-in-bracket>
             </div>
             <div class="final-matches">
-                <player-in-bracket name="Sowoo" score="11" id="final-player1" class="player-in-bracket-tournament-win"></player-in-bracket>
+                <player-in-bracket name="Sowoo" score="11" id="final-player1" class="player-in-bracket-tournament-win right-players"></player-in-bracket>
                 <player-in-bracket name="Sowoo" score="9" id="final-player2" class="player-in-bracket-basic"></player-in-bracket>
             </div>
             <div class="right-matches">
-                <player-in-bracket name="Sowoo" score="7" id="right-height-player1" class="player-in-bracket-basic"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="11" id="right-height-player2" class="player-in-bracket-game-win"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="8" id="right-height-player3" class="player-in-bracket-basic"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="11" id="right-height-player4" class="player-in-bracket-game-win"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="11" id="right-height-player5" class="player-in-bracket-game-win"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="1" id="right-height-player6" class="player-in-bracket-basic"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="6" id="right-height-player7" class="player-in-bracket-basic"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="11" id="right-height-player8" class="player-in-bracket-game-win"></player-in-bracket>
+<player-in-bracket name="Sowoo" score="11" id="right-height-player1" class="player-in-bracket-game-win right-players"></player-in-bracket>
+                <player-in-bracket name="Hleung" score="1" id="right-height-player2" class="player-in-bracket-basic right-players"></player-in-bracket>
+                <player-in-bracket name="Acarlott" score="7" id="right-height-player3" class="player-in-bracket-basic right-players"></player-in-bracket>
+                <player-in-bracket name="Pgouasmi" score="11" id="right-height-player4" class="player-in-bracket-game-win right-players"></player-in-bracket>
+                <player-in-bracket name="Chonorat" score="8" id="right-height-player5" class="player-in-bracket-basic right-players"></player-in-bracket>
+                <player-in-bracket name="Eguelin" score="11" id="right-height-player6" class="player-in-bracket-game-win right-players"></player-in-bracket>
+                <player-in-bracket name="Natterie" score="11" id="right-height-player7" class="player-in-bracket-game-win right-players"></player-in-bracket>
+                <player-in-bracket name="Tduprez" score="9" id="right-height-player8" class="player-in-bracket-basic right-players"></player-in-bracket>
                 
-                <player-in-bracket name="Sowoo" score="11" id="right-four-player1" class="player-in-bracket-game-win"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="7" id="right-four-player2" class="player-in-bracket-basic"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="8" id="right-four-player3" class="player-in-bracket-basic"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="11" id="right-four-player4" class="player-in-bracket-game-win"></player-in-bracket>
+                <player-in-bracket name="XX_test_duXX" score="11" id="right-four-player1" class="player-in-bracket-game-win right-players"></player-in-bracket>
+                <player-in-bracket name="WWWWWWWWWWWW" score="7" id="right-four-player2" class="player-in-bracket-basic right-players"></player-in-bracket>
+                <player-in-bracket name="Sowoo" score="8" id="right-four-player3" class="player-in-bracket-basic right-players"></player-in-bracket>
+                <player-in-bracket name="Sowoo" score="11" id="right-four-player4" class="player-in-bracket-game-win right-players"></player-in-bracket>
                 
-                <player-in-bracket name="Sowoo" score="11" id="right-two-player1" class="player-in-bracket-game-win"></player-in-bracket>
-                <player-in-bracket name="Sowoo" score="8" id="right-two-player2" class="player-in-bracket-basic"></player-in-bracket>
+                <player-in-bracket name="Sowoo" score="11" id="right-two-player1" class="player-in-bracket-game-win right-players"></player-in-bracket>
+                <player-in-bracket name="Ceci est un tes" score="8" id="right-two-player2" class="player-in-bracket-basic right-players"></player-in-bracket>
             </div>
 		`;
     }
