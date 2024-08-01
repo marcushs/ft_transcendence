30a7e482fe3c6b17ea0b9153a2a312935da5969f
Shellks <nagabtc@gmail.com>
2024-08-01 10:09:52 +0200

    feat(friends): add get_friend_request_status method to getfriendshipstatus python class

--- src/requirements/backend/friends_service/friends_app/views.py
+++ src/requirements/backend/friends_service/friends_app/views.py
@@ -1,9 +1,11 @@
 from typing import Any
 from django.http import JsonResponse
 from django.contrib.auth.models import AnonymousUser
-from .utils.friends_utils import fr
+from django.contrib.auth import get_user_model
 from django.views import View
+from .models import FriendList, FriendRequest
 from enum import Enum
+import json
 
 class FriendRequestStatus(Enum):
     NO_REQUEST_SENT = -1
@@ -12,21 +14,50 @@ class FriendRequestStatus(Enum):
     
 
 class GetFriendShipStatus(View):
-    def __init__(self, **kwargs: Any) -> None:
-        super().__init__(**kwargs)
+    def __init__(self):
+        super()
         
     def get(self, request):
-        is_self = True
-        is_friend = False
         if isinstance(request.user, AnonymousUser):
-            return JsonResponse({'status': 'error', 'message': 'User not found'}, status=400)
-        target_user = request.GET.get('q', '')
-        if target_user != request.user:
-            is_self = False
-            
-    def get_friend_list(target_user):
-        try:
-            friend_list =  friends_utils.
-        except:
-            pass
+            return JsonResponse({'message': 'You are not logged in'}, status=400)
+        self.init(request=request)
+        response = self.check_data(user=request.user)
+        if response['status'] != 200:
+            return JsonResponse({'message': response['message']}, status=response['status'])
+        return self.get_friend_request_status(user=request.user)
         
+    def  init(self, request):
+        self.is_self = True
+        self.User = get_user_model()
+        self.target_username = request.GET.get('q', '')
+        self.friend_list = FriendList.objects.filter(user=request.user).first()
+    
+    def check_data(self, user):
+        if not self.friend_list:
+            return {'status': 404, 'message': 'Friend list not found'}
+        if not self.target_username:
+            return {'status': 400, 'message': 'No target user provided'}
+        try:
+            target_user = self.User.objects.get(username=self.target_username)
+        except self.User.DoesNotExist:
+            return {'status': 404, 'message': 'Target user does not exist'}
+        if user != target_user:
+            self.is_self = False
+        return {'status': 200}
+
+    def get_friend_request_status(self, user):
+        if self.is_self == True:
+            return JsonResponse({'status': 'self'}, status=200)
+        if self.friend_list.is_mutual_friend(friend=self.target_username):
+            return JsonResponse({'status': 'friend'}, status=200)
+        try:
+            FriendRequest.objects.get(sender=user, receiver=self.target_username, is_active=True):
+            return JsonResponse({'status': 'pending', 'sender': 'you'}, status=200)
+        except FriendRequest.DoesNotExist:
+            pass
+        try:
+            if FriendRequest.objects.get(sender=self.target_username, receiver=user, is_active=True):
+                return JsonResponse({'status': 'pending', 'sender': 'target'}, status=200)
+        except FriendRequest.DoesNotExist:
+            pass
+        return JsonResponse({'status': 'unknown'}, status=200)
