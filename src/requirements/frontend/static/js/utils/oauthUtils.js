b62068e537665cdc5406c323bfbfd46308b1f866
Hok sum Leung <hleung@z1r8p4.42lyon.fr>
2024-08-01 14:25:18 +0200

    feat(oauth): refactor oauthRedirect.js, add function oautRedirectCallback

--- src/requirements/frontend/static/js/utils/oauthUtils.js
+++ src/requirements/frontend/static/js/utils/oauthUtils.js
@@ -8,19 +8,52 @@ const config = {
 		},
 		credentials: 'include', // Needed for send cookie
 	};
+	
+export async function oauthRedirectCallback() {
+	const urlParams = new URLSearchParams(window.location.search);
+	const code = urlParams.get('code');
+	const state = urlParams.get('state');
+	const status_text = document.getElementById('status');
 
-export async function redirectToOauth() {
-	try {
-		const res = await fetch(`http://localhost:8003/oauth/login/`, config);
-		const data = await res.json();
-		console.log(data);
-		window.location.replace(data.url);
-	} catch (error) {
-		console.log(error);
+	// Check if there are code and state in query param, request comes from 42 oauth
+	if (code && state) {
+		const data = await handleOauthCallback(code, state);
+		
+		if (data.status === 'Success') {
+			// if handleOauthCallback success
+			const user_data = await accessResource();
+			if (!user_data || user_data.status === 'Error') {
+				status_text.textContent = `Error: ${user_data ? user_data.message : 'Fetch failed'}`
+				setTimeout(() => window.location.href = '/login', 2000);
+				return ;
+			}
+			console.log('oauth redirect page:')
+			console.log(user_data);
+			status_text.textContent = 'Successfully logged in';
+		} else {
+			// if handleOauthCallback error
+			status_text.textContent = `Error: ${data.message}`;
+			setTimeout(() => window.location.href = '/login', 2000);
+		}
+	} else {
+		// No query params, not from 42 oauth
+		status_text.textContent = 'Error: Invalid request';
+		setTimeout(() => window.location.href = '/login', 2000);
 	}
 }
 
-export async function handleOauthCallback(code, state) {
+export async function redirectToOauth() {
+	try {
+	const res = await fetch(`http://localhost:8003/oauth/login/`, config);
+	const data = await res.json();
+	console.log(data);
+	window.location.replace(data.url);
+} catch (error) {
+	console.log(error);
+}
+}
+
+async function handleOauthCallback(code, state) {
 	try {
 		const res = await fetch(`http://localhost:8003/oauth/redirect/?code=${code}&state=${state}`, config);
 		const data = await res.json();
@@ -31,7 +64,7 @@ export async function handleOauthCallback(code, state) {
 	}
 }
 
-export async function accessResource() {
+async function accessResource() {
 	try {
 		const res = await fetch(`http://localhost:8003/oauth/access_resource/`, config);
 		const data = await res.json();
@@ -41,3 +74,5 @@ export async function accessResource() {
 		return null;
 	}
 }
+
+
--- src/requirements/frontend/static/js/views/oauthRedirect.js
+++ src/requirements/frontend/static/js/views/oauthRedirect.js
@@ -1,4 +1,4 @@
-import { handleOauthCallback, accessResource } from "../utils/oauthUtils.js";
+import { oauthRedirectCallback } from "../utils/oauthUtils.js";
 
 export default () => {
 	const html = `
@@ -6,38 +6,7 @@ export default () => {
 		<p id="status">Processing your login...</p>
 	`;
 
-	setTimeout(async () => {
-		const urlParams = new URLSearchParams(window.location.search);
-		const code = urlParams.get('code');
-		const state = urlParams.get('state');
-		const status_text = document.getElementById('status');
-	
-		// Check if there are code and state in query param, request comes from 42 oauth
-		if (code && state) {
-			const data = await handleOauthCallback(code, state);
-			
-			if (data.status === 'Success') {
-				// if handleOauthCallback success
-				const user_data = await accessResource();
-				if (!user_data || user_data.status === 'Error') {
-					status_text.textContent = `Error: ${user_data ? user_data.message : 'Fetch failed'}`
-					setTimeout(() => window.location.href = '/login', 2000);
-					return ;
-				}
-				console.log('oauth redirect page:')
-				console.log(user_data);
-				status_text.textContent = 'Successfully logged in';
-			} else {
-				// if handleOauthCallback error
-				status_text.textContent = `Error: ${data.message}`;
-				setTimeout(() => window.location.href = '/login', 2000);
-			}
-		} else {
-			// No query params, not from 42 oauth
-			status_text.textContent = 'Error: Invalid request';
-			setTimeout(() => window.location.href = '/login', 2000);
-		}
-	}, 0);
+	setTimeout(oauthRedirectCallback, 0);
 
 	return html;
 }
