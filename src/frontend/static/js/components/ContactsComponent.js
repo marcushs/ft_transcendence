e30bc7c400ae08c18e49401a697eac1ba0178da9
Shellks <nagabtc@gmail.com>
2024-08-14 10:14:28 +0200

    feat(friends): prototype design for contact menu done

--- src/frontend/static/js/components/ContactsComponent.js
+++ src/frontend/static/js/components/ContactsComponent.js
@@ -15,32 +15,33 @@ class FriendsMenuComponent extends HTMLElement {
             <div class='contact-menu'>
                 <div class='top-bar-contacts-menu'>
                     <div class='add-contact'>
-                        <img src='../../assets/add_friend.png' alt='add-friend-icon'></img>
+                        <img src='../../assets/add_friend.svg' alt='add-friend-icon'></img>
                     </div>
-                    <div class='search-contact'>
-                        <form action="#" autocomplete="off">
+                    <form action="#" autocomplete="off">
                         <img src="../../assets/search-bar-icon.svg" alt="search-bar-icon" class="search-bar-icon">
-                        <div class="search-contact-input-container">
-                            <input type="text" placeholder="Search contact" id="search-contact-input"/>
-                        </div>
-                        </form>
-                    </div>
+                        <input type="text" placeholder="Search contacts" id="search-contact-input"/>
+                    </form>
                 </div>
                 <div class='contact-list-menu'>
                     <p class='contact-summary'></p>
                     <ul class="contact-list-result"></ul>
+                    <p class='pending-contact-summary'></p>
+                    <ul class="pending-contact-list-result"></ul>
                 </div>
             </div>
         `
 
         this.addContact = this.querySelector('.add-contact');
         this.contactList = this.querySelector('.contact-list-result');
+        this.pendingContactList = this.querySelector('.pending-contact-list-result');
         this.contactMenuDiv = this.querySelector('.contact-menu');
         this.contactSummary = this.querySelector('.contact-summary');
+        this.pendingContactSummary = this.querySelector('.pending-contact-summary');
         this.searchContactInput = this.querySelector('#search-contact-input');
         this.contactBottomNavDiv = this.querySelector('.bottom-nav-contacts');
 
         this.contactMenuDiv.style.display = 'none';
+        this.pendingContactList.style.display = 'none';
     }
 
     connectedCallback() {
@@ -52,14 +53,22 @@ class FriendsMenuComponent extends HTMLElement {
         this.contactBottomNavDiv.addEventListener('click', () => {
             this.contactMenuDiv.style.display = this.contactMenuDiv.style.display === 'none' ? 'block' : 'none';
         });
+        this.pendingContactSummary.addEventListener('click', () => {
+            this.pendingContactList.style.display = this.pendingContactList.style.display === 'none' ? 'block' : 'none';
+        });
+        this.contactSummary.addEventListener('click', () => {
+            this.contactList.style.display = this.contactList.style.display === 'none' ? 'block' : 'none';
+        });
     }
 
     async displayContactList() {
         // const contacts = await this.getContacts();
         const contacts = null;
         if (!contacts) {
-            this.contactSummary.innerHTML = `<p>Friends - 0/0</p>`;
+            this.contactSummary.innerHTML = `<p>Contacts  -  0/0</p>`;
             this.contactList.innerHTML = `No contacts found...`
+            this.pendingContactSummary.innerHTML = `<p>Pending  -  0</p>`;
+            this.pendingContactList.innerHTML = `No pending request...`
         } else {
             this.contactSummary.innerHTML = `<p>Friends - ${contacts.onlineContactCount}/${contacts.contactCount}</p>`;
             contacts.forEach(user => {
