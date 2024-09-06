a6074cfe0099d100e00549c2ab7e5e1ebdaebf0d
TheoDuprez <sylvie.theo.mathieu@gmail.com>
2024-09-06 17:34:57 +0200

    feat(date display): the date can now be displayed on notifications

--- src/frontend/static/js/components/Notifications/PendingContactRequestNotificationComponent.js
+++ src/frontend/static/js/components/Notifications/PendingContactRequestNotificationComponent.js
@@ -9,14 +9,41 @@ class PendingContactRequestNotificationComponent extends HTMLElement {
 	}
 
 	initializeComponent() {
+		const date = new Date(this.notificationObj.created_at);
+
+		// this.convertDateFormat(date);
 		this.innerHTML = `
 			<li>			
 				<p>${this.notificationObj.message}</p>
 				<i class="fa-solid fa-check"></i>
 				<i class="fa-solid fa-xmark"></i>
+				<p class="notification-date">${this.convertDateFormat(date)}</p>
 				<hr>
 			</li>
 		`;
+	}
+
+
+	convertDateFormat(date) {
+		const currentDate = new Date();
+		const diffInMs = currentDate - date;
+
+		const diffObj = {
+			seconds: Math.floor((diffInMs / 1000) % 60),
+			minutes: Math.floor((diffInMs / (1000 * 60)) % 60),
+		    hours: Math.floor((diffInMs / (1000 * 60 * 60)) % 24),
+		    days: Math.floor((diffInMs / (1000 * 60 * 60 * 24)) % 7)
+		}
+
+		if (diffObj.days >= 7)
+			return `${date.toLocaleDateString()}`;
+		if (diffObj.days >= 1)
+			return `${diffObj.days}d`;
+		if (diffObj.hours >= 1)
+			return `${diffObj.hours}h`;
+		if (diffObj.minutes >= 1)
+			return `${diffObj.minutes}m`;
+		return `${diffObj.seconds}s`;
 	}
 
 
