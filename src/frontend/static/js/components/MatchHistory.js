68f323b866c03946b06c12feec8b2e5d9e72907a
TheoDuprez <sylvie.theo.mathieu@gmail.com>
2024-10-29 20:42:16 +0100

    feat(history): history frontend has been created, need to connect it to backend

--- src/frontend/static/js/components/MatchHistory.js
+++ src/frontend/static/js/components/MatchHistory.js
@@ -4,7 +4,63 @@ class MatchHistory extends HTMLElement {
 
         this.innerHTML = `
             <div class="match-history-container">
-                <h1>History</h1>
+                <h1>Match history</h1>
+                <ul class="history-list-container">
+                    <li class="win">
+                        <div class="type">
+                            <p>Ranked</p>
+                            <div class="line"></div>
+                        </div>
+                        <div class="content">
+                            <p class="vs">VS</p>
+                            <p class="name">WWWWWWWWWWWW</p>
+                            <p class="score">11 - 9</p>
+                        </div>
+                    </li>
+                    <li class="win">
+                        <div class="type">
+                            <p>Ranked</p>
+                            <div class="line"></div>
+                        </div>
+                        <div class="content">
+                            <p class="vs">VS</p>
+                            <p class="name">tduprez</p>
+                            <p class="score">11 - 9</p>
+                        </div>
+                    </li>
+                    <li class="lose">
+                        <div class="type">
+                            <p>Unranked</p>
+                            <div class="line"></div>
+                        </div>
+                        <div class="content">
+                            <p class="vs">VS</p>
+                            <p class="name">acarlott</p>
+                            <p class="score">11 - 9</p>
+                        </div>
+                    </li>
+                    <li class="win">
+                        <div class="type">
+                            <p>Tournament</p>
+                            <div class="line"></div>
+                        </div>
+                        <div class="content">
+                            <p class="tournament-name">Sowoo's tournamentttdfokghnfdoihgjdfh</p>
+                            <p class="score">12-16th</p>
+                        </div>
+                    </li>
+                    <li class="lose">
+                        <div class="type">
+                            <p>Ranked</p>
+                            <div class="line"></div>
+                        </div>
+                        <div class="content">
+                            <p class="vs">VS</p>
+                            <p class="name">hleung</p>
+                            <p class="score">11 - 9</p>
+                        </div>
+                    </li>
+                </ul>
             </div>
         `
 
