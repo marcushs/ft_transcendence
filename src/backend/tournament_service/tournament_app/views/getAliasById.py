2ad44700352d0efac69194fb556a56c6eb138a8c
Hok sum Leung <hleung@z1r3p3.42lyon.fr>
2024-11-21 18:07:06 +0100

    fix(tournament): show alias instead of usernamen in game intro for tournament games

---
+++ src/backend/tournament_service/tournament_app/views/getAliasById.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AnonymousUser
+from django.http import JsonResponse
+from django.views import View
+from ..models import User
+
+ 
+class getAliasById(View):
+	def __init__(self):
+		super().__init__
+
+	def get(self, request):
+		try:
+			if isinstance(request.user, AnonymousUser): 
+				return JsonResponse({'message': 'unknownUser'}, status=400)
+			player_id = str(request.GET.get('player_id'))
+			player = User.objects.get(id=player_id)
+			return JsonResponse({'alias': player.alias}, status=200)
+		except User.DoesNotExist:
+			return JsonResponse({'message': 'unknownUser'}, status=400)
+		except Exception as e:
+			print(f'Error: {str(e)}')
+			return JsonResponse({"message": str(e)}, status=400)
